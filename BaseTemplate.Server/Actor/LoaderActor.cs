1051632d4bc2ba08574e3d13b19612e427d934c1
yoonsung.park <yspak82@gmail.com>
2021-11-01 14:00:45 +0900

    add(server):서비스 템플릿 추가함

---
+++ BaseTemplate.Server/Actor/LoaderActor.cs
@@ -0,0 +1,50 @@
+﻿using Akka.Actor;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTemplate.Server
+{
+    public class LoaderActor : SeriLoggerActor, IDisposable
+    {
+        
+        TestConfig config;
+        Dictionary<string, IActorRef> chileActors = new Dictionary<string, IActorRef>();
+
+
+        JsonSerializerSettings jsonSetting;
+        string lastEndInspection = string.Empty;
+        public LoaderActor(TestConfig postProcessorConfig)
+        {
+            this.config = postProcessorConfig;
+            this.jsonSetting = new JsonSerializerSettings()
+            {
+                Error = delegate (object sender, ErrorEventArgs args)
+                {
+                    this.logger.Error(args.ErrorContext.Error.Message);
+                    args.ErrorContext.Handled = true;
+                }
+            };            
+            ReceiveAkkaMessageEvents();
+        }
+
+
+        private void ReceiveAkkaMessageEvents()
+        {            
+            Receive<string>(msg =>
+            {
+                Console.WriteLine($"{Sender}:{msg}");                
+            });
+        }
+
+        public void Dispose()
+        {
+ 
+        }
+    }
+}
--- BaseTemplate.Server/Program.cs
+++ BaseTemplate.Server/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace BaseTemplate.Server
 {
-    class Program
+    class Program : ProgramBase
     {
         static void Main(string[] args)
         {
+            SetConsoleCtrlHandler(Handler, true);
+            service = new MainService();
+            var host = HostFactory.New(x =>
+            {
+                x.Service<IMainService>(s =>
+                {
+                    s.ConstructUsing(name => service);
+                    s.WhenStarted(tc => tc.Start(args));
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                //x.SetDescription("Server.HG.Master");
+                //x.SetDisplayName("Server.HG.Master");
+                //x.SetServiceName("Server.HG.Master");
+            });
+
+            host.Run();
         }
     }
 }
